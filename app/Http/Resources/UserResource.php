aa8dff02b90a9e4368496e01564c0b64b13845a1
TheRealAliz <ali.tmd24@gmail.com>
2026-06-13 19:46:20 +0330

    feat: add UserResource to standardize user API responses

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'full_name' => $this->full_name,
+            'username' => $this->username,
+            'email' => $this->email,
+            'mobile' => $this->mobile,
+            'mobile_verified_at' => $this->mobile_verified_at?->toDateTimeString(),
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+        ];
+    }
+}
