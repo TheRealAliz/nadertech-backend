2a3209c7e50a50d8f937e6b5ce3697d39674df36
TheRealAliz <ali.tmd24@gmail.com>
2026-06-20 16:17:25 +0330

    fix: change ProjectServiceController function return type to JsonResponse for integrity

--- app/Http/Controllers/Api/ProjectServiceController.php
+++ app/Http/Controllers/Api/ProjectServiceController.php
@@ -5,34 +5,47 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProjectRequests\ProjectServiceResource;
 use App\Models\ProjectService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProjectServiceController extends Controller
 {
-    public function index(): ProjectServiceResource|AnonymousResourceCollection
+    public function index(): JsonResponse
     {
         $services = ProjectService::query()
             ->orderBy('parent_id')
             ->orderBy('sort_order')
             ->get();
 
-        return ProjectServiceResource::collection($services);
+        return response()->json([
+            'data' => [
+                'services' => ProjectServiceResource::collection($services),
+            ]
+        ]);
     }
 
-    public function show(ProjectService $projectService): ProjectServiceResource
+    public function show(ProjectService $projectService): JsonResponse
     {
         $projectService->load('children');
 
-        return new ProjectServiceResource($projectService);
+        return response()->json([
+            'data' => [
+                'service' => new ProjectServiceResource($projectService),
+            ]
+        ]);
     }
 
-    public function tree(): ProjectServiceResource|AnonymousResourceCollection
+    public function tree(): JsonResponse
     {
         $services = ProjectService::with('children')
             ->parents()
             ->orderBy('sort_order')
             ->get();
 
-        return ProjectServiceResource::collection($services);
+        return response()->json([
+            'data' => [
+                'services' => ProjectServiceResource::collection($services),
+            ]
+        ]);
     }
 }
