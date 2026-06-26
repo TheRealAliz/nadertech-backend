ca73180accf4e2e6c7da7038d9332c8d31a748a4
TheRealAliz <ali.tmd24@gmail.com>
2026-06-26 14:24:36 +0330

    seed: add RolesAndPermissionsSeeder

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        /*
+        |--------------------------------------------------------------------------
+        | Permissions
+        |--------------------------------------------------------------------------
+        */
+
+        $permissions = [
+
+            // Users
+            'admin.users.view',
+            'admin.users.view.single',
+
+            // Banners
+            'admin.banners.show',
+            'admin.banners.create',
+            'admin.banners.update',
+            'admin.banners.delete',
+            'admin.banners.update.status',
+            'admin.banners.update.image',
+            'admin.banners.reorder',
+
+            // Articles
+            'admin.articles.view',
+            'admin.articles.create',
+            'admin.articles.update',
+            'admin.articles.delete',
+            'admin.articles.archived.view',
+            'admin.articles.update.status',
+            'admin.articles.update.thumbnail',
+
+            // Lotteries
+            'admin.lotteries.create',
+            'admin.lotteries.update',
+            'admin.lotteries.delete',
+            'admin.lotteries.entries.view',
+            'admin.lotteries.draw',
+            'admin.lotteries.winners.view',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission,
+                'guard_name' => 'admin',
+            ]);
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Roles
+        |--------------------------------------------------------------------------
+        */
+
+        $superAdmin = Role::firstOrCreate([
+            'name' => 'super-admin',
+            'guard_name' => 'admin',
+        ]);
+
+        /*
+        |--------------------------------------------------------------------------
+        | Assign Permissions
+        |--------------------------------------------------------------------------
+        */
+
+        $superAdmin->syncPermissions(Permission::all());
+
+        Admin::find(1)->assignRole('super-admin');
+    }
+}
