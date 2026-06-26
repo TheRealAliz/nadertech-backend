e56f637d2c850497c227f2a51968a874a99c80e3
TheRealAliz <ali.tmd24@gmail.com>
2026-06-26 12:29:06 +0330

    feat&docs: add BannerResource for client users and  update OpenApiSpec docs for AdminBannerResource

---
+++ app/Http/Resources/Banner/BannerResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\Banner;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class BannerResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'image' => $this->image ? Storage::disk($this->image) : null,
+            'alt' => $this->alt,
+            'link' => $this->link,
+        ];
+    }
+}
